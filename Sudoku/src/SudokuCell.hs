95328dea30e71baed812eb3eaf2ae03bed052c81
ClauCalv <38563172+ClauCalv@users.noreply.github.com>
2019-08-20 14:19:24 -0300

    Mais trabalho hehe

--- Sudoku/src/SudokuCell.hs
+++ Sudoku/src/SudokuCell.hs
@@ -1,7 +1,22 @@
-data Cell = FixedCell Int | OpenCell [Int]
+data Cell = FixedCell Char | OpenCell [Char] -- TEREMOS QUE USAR CHAR EM VEZ DE INT SE QUISERMOS ALGO MAIOR QUE 9x9
+
+newCell :: OpenCell
+newCell = [0..maxChar] -- temos que definir quem são os nossos chars, e como informar.
+
 instance Show Cell where
-  show (FixedCell a) = show a
-  show (OpenCell _)  = "_"
+    show (FixedCell a) = [a]
+    show (OpenCell _)  = "_"
+
+instance Read Cell where
+    read s | s == "_"       = newCell
+    read s | length s == 1  = FixedCell (head s)
+    read s                  = OpenCell s
+
+showOpen :: Cell -> String
+showOpen (FixedCell a) = [a]
+showOpen (OpenCell xs) = xs
+
+readOpen :: String -> Cell -- TODO talvez todos os read_Opens precisem informar tamanho
 
 type Row = [Cell]
 
@@ -9,5 +24,18 @@ showRow :: Row -> String
 showRow [] = ""
 showRow xs = concatMap ((++ " ") . show) xs ++ "\n"
 
+showRowOpen :: Row -> String
+showRowOpen [] = ""
+showRowOpen xs = concatMap ((++ " ") . showOpen) xs ++ "\n"
+
+readRow :: String -> Row -- TODO
+readRowOpen :: String -> Row -- TODO
+
 type Table = [Row]
-data TableConfig = TableConfig {currTable :: Table, rectLen :: Int, rectWid :: Int} -- nosso estado
+
+showTable :: Table -> String
+showTableOpen :: Table -> String
+readTable :: String -> Table
+readTableOpen :: String -> Table
+
+data TableConfig = TableConfig {currTable :: Table, rectLen :: Int, rectWid :: Int}
