294ce6dd584b3e42804852ec5374005cc8eef0b1
ClauCalv <38563172+ClauCalv@users.noreply.github.com>
2019-08-24 17:21:53 -0300

    Compilou!!!

--- Sudoku/src/Lib.hs
+++ Sudoku/src/Lib.hs
@@ -20,32 +20,36 @@ isFixed :: Cell -> Bool -- duh
 isFixed (FixedCell _) = True
 isFixed (OpenCell _) = False
 
-solve :: GameState (Maybe Table)
-solve = do
-          nextTable <- findFixPoint
-          case nextTable of
-            Nothing               -> return Nothing
-            (Just a) | isSolved a -> return (Just a)
-            (Just a)  -> do
-                          let (next1, next2) = splitTableAtMin 3 3 a
-                          solve1 <- solve next1
-                          solve2 <- solve next2 -- Será que o lazyness cuida de só resolver o 2 se o 1 der nothing??
-                          if isNothing solve1 then return solve2 else return solve1
+solve :: Table -> Int -> Int -> Maybe Table
+solve table len wid = evalState solve' (TableConfig table len wid)
+
+solve' ::  GameState (Maybe Table)
+solve' = do
+    currState <- get
+    let (len, wid) = (rectLen currState, rectWid currState)
+    nextTable <- findFixPoint
+    case nextTable of
+        Nothing               -> return Nothing
+        (Just a) | isSolved a -> return (Just a)
+        (Just a)  -> do
+            let (next1, next2) = splitTableAtMin len wid a
+            put ( currState { currTable = next1 } )
+            solve1 <- solve'
+            if isJust solve1 then return solve1 else put ( currState { currTable = next2 } ) >> solve'
 
 findFixPoint :: GameState (Maybe Table) -- aqui eu recomendo que olhe o artigo que te mandei antes de tentar entender
 findFixPoint = do
     config <- get
     let table = currTable config
-    table' <- runTable table -- runTable altera o estado, por isso pode retornar que já tá atualizado
-    if table /= table' then findFixPoint else return table' -- nothing == nothing, aí quem chamou se vira
+    table' <- runTable -- runTable altera o estado, por isso pode retornar que já tá atualizado
+    if table' /= (Just table) then findFixPoint else return table'
 
-runTable :: Maybe Table -> GameState (Maybe Table)
-runTable Nothing  = return Nothing
-runTable table = do
+runTable :: GameState (Maybe Table)
+runTable = do
     config <- get
-    let (len, wid) = (rectLen config, rectWid config)
-    let table' = checkForRects len wid $ checkForCols $ checkForRows table -- provavelmente o 3D só vai mexer aqui
-    put ( config { currTable = fromMaybe (fromJust table) table' } ) -- atualiza a tabela velha com a nova
+    let (len, wid, table) = (rectLen config, rectWid config, currTable config)
+    let table' = checkForRects len wid . checkForCols . checkForRows $ Just table -- provavelmente o 3D só vai mexer aqui
+    put ( config { currTable = fromMaybe table table' } ) -- atualiza a tabela velha com a nova
     return table' -- devolve como parâmetro. Se não quisesse comparar, poderia ter sido return ()
 
 checkForRows :: Maybe Table -> Maybe Table -- traverse f x nada mais é que (sequence $ map f x)
@@ -73,7 +77,6 @@ checkRow row = traverse checkCells row -- por enquanto ta fazendo um passo só. 
             xs     -> Just (OpenCell xs)
 
 splitTableAtMin :: Int -> Int -> Table -> (Table, Table)
-splitTableAtMin a b c = (c,c)
 splitTableAtMin len wid table =
     let (index, cell, cell') = splitMinCell table -- descobre onde quebrar
     in (replace index cell table (len+wid), replace index cell' table (len+wid)) -- substitui a quebra
@@ -81,7 +84,7 @@ splitTableAtMin len wid table =
         flatTable table = zip [0..] (concat table)
 
         splitMinCell table = splitCell $ minCell table
-        minCell table = minimumBy (compare `on` (cellSize.snd)) $ filter (not.isFixed.snd) flatTable table -- ODEIO USAR (compare `on` x)
+        minCell table = minimumBy (compare `on` (cellSize.snd)) $ filter (not.isFixed.snd) $ flatTable table -- ODEIO USAR (compare `on` x)
         cellSize (FixedCell _) = 1
         cellSize (OpenCell xs) = length xs
         splitCell (i, FixedCell _) = error "?" -- compilador pede, fazer oq...
