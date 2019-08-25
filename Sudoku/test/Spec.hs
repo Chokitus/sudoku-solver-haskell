5a73496deb515bad6d542b08c081fa7a4350e7a9
Victor Bressani de Mello <victor.bremello@hotmail.com>
2019-08-25 19:07:35 -0300

    Maaaaais testes

--- Sudoku/test/Spec.hs
+++ Sudoku/test/Spec.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import SudokuCell
+import Lib
 import Test.Tasty
 import Test.Tasty.HUnit 
 
@@ -10,7 +11,7 @@ main =
 
 
 tests :: TestTree
-tests = testGroup "Testes da Atividade 4" [celulaTest, rowTest, tableTest]
+tests = testGroup "Testes da Atividade 4" [celulaTest, rowTest, tableTest, helperTest]
 --SudokuCell
 -- test_e1_1 = TestCase (do
 --                      x <- diff "test/1.txt" "test/2.txt"
@@ -51,3 +52,33 @@ tableTest = testGroup "Leitura e escrita de Tabelas"
         ]
 
 -- Lib
+
+sampleRowOpen :: Row
+sampleRowOpen = [OpenCell [5,4,3,2,1], FixedCell 1, FixedCell 2, FixedCell 3, FixedCell 4]
+
+sampleRowOpen2 :: Row
+sampleRowOpen2 = [OpenCell [5,4,3,2,1], OpenCell [5,4,3,2,1], FixedCell 2, FixedCell 3, FixedCell 4]
+
+sampleRowOpen2_after :: Row
+sampleRowOpen2_after = [OpenCell [5,1], OpenCell [5,1], FixedCell 2, FixedCell 3, FixedCell 4]
+
+sampleRowFixed :: Row
+sampleRowFixed = [FixedCell 5, FixedCell 1, FixedCell 2, FixedCell 3, FixedCell 4]
+
+sampleTable :: Table
+sampleTable = readTable 4 "0 2 3 4 3 4 0 2 2 1 4 3 4 3 0 0"
+
+sampleTableCol :: Table
+sampleTableCol = [[FixedCell 1, FixedCell 2, FixedCell 3     , FixedCell 4],
+                  [FixedCell 3, FixedCell 4, OpenCell [1,2], FixedCell 2],
+                  [FixedCell 2, FixedCell 1, FixedCell 4     , FixedCell 3],
+                  [FixedCell 4, FixedCell 3, OpenCell [1,2], FixedCell 1]]
+
+helperTest = testGroup "Métodos auxiliares"
+          [
+            testCase "isFixed - Open" (assertEqual "Não é fixo" False (isFixed (OpenCell [1,2,3]))),
+            testCase "isFixed - Fixed" (assertEqual "É fixo" True (isFixed (FixedCell 1))),
+            testCase "checkRow - Única possível" (assertEqual "Encontrou única célula sem solução e resolveu" (Just sampleRowFixed) (checkRow sampleRowOpen)),
+            testCase "checkRow - Filtro" (assertEqual "Somente tirou possibilidades incongruentes" (Just sampleRowOpen2_after) (checkRow sampleRowOpen2)),
+            testCase "checkForCols" (assertEqual "Retirou possibilidades e marcou um valor" (Just sampleTableCol) (checkForCols (Just sampleTable)))
+          ]
